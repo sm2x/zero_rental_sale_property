03173c437a067e277ce5dad342ef866336c40d17
ZeroSystems <sales@erpzero.com>
2019-10-01 14:01:04 +0200

    Update property.py

--- zero_rental_sale_property/models/property.py
+++ zero_rental_sale_property/models/property.py
@@ -1,5 +1,6 @@
-from datetime import datetime
-from datetime import datetime
+from datetime import datetime, date, time, timedelta
+from pytz import timezone
+import time
 from odoo.exceptions import ValidationError
 from odoo import models, fields, api, _, exceptions
 from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
