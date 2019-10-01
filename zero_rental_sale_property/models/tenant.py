64fcbc601d8778c018f677d5567e013d826999a2
ZeroSystems <sales@erpzero.com>
2019-10-01 14:04:27 +0200

    Update tenant.py

--- zero_rental_sale_property/models/tenant.py
+++ zero_rental_sale_property/models/tenant.py
@@ -1,5 +1,9 @@
-from openerp import models, fields, api
-
+from datetime import datetime, date, time, timedelta
+from pytz import timezone
+import time
+from odoo.exceptions import ValidationError
+from odoo import models, fields, api, _, exceptions
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
 
 class Tenant(models.Model):
     _inherit = 'res.partner'
