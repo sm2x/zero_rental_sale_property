97c619b34b177ff37888fb599bc8146db8ba86ef
ZeroSystems <sales@erpzero.com>
2019-10-08 02:38:08 +0200

    Update __manifest__.py

--- zero_rental_sale_property/__manifest__.py
+++ zero_rental_sale_property/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    "name": "zero_rental_sale_property",
+    'name': 'zero_rental_sale_property',
     'version': '10.1.0',
     "summary": "rental and sales properaty management ",
     'description': """this App for Renting and Sales Properaty management full cycle integrated with odoo 10 (sales , inventory, ecommerce and accouting)-tested on odoo community and enterprise 10-Support English and Arabic interface""",
@@ -9,7 +9,7 @@
     'company': 'Zero for Information Systems',
     'website': "https://www.erpzero.com",
     'email': "sales@erpzero.com",
-    "data": [
+    'data': [
         'report/report_contract.xml',
         'report/contract_views.xml','views/property.xml',
         'views/tenant.xml',
